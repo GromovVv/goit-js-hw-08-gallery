b065d41d9366e90b702a397202e145deb5044ad3
Pavel Kuts <pavel.kuts@gmail.com>
2021-01-31 23:04:43 +0200

    ready!)

---
+++ js/task.js
@@ -0,0 +1,54 @@
+import images from './gallery-items.js';
+
+const listRef = document.querySelector('.js-gallery');
+
+const createList = (image) => {
+    const listEl = document.createElement('li');
+    listEl.classList.add('gallery__item');
+
+    const linkEl = document.createElement('a');
+    linkEl.classList.add('gallery__link');
+    linkEl.setAttribute('href', image.original);
+
+    const imgEl = document.createElement('img');
+    imgEl.classList.add('gallery__image');
+    imgEl.setAttribute('src', image.preview);
+    imgEl.setAttribute('alt', image.description);
+    imgEl.dataset.source = image.original;
+
+    linkEl.appendChild(imgEl);
+    listEl.appendChild(linkEl);
+    
+    return listEl;
+}
+
+const arr = images.map((image) => createList(image));
+
+listRef.append(...arr);
+
+
+let bigImgUrl = '';
+const modal = document.querySelector('.js-lightbox');
+const modalImg = document.querySelector('.lightbox__image');
+
+const getBigUrl = (event) => {
+    event.preventDefault();
+    bigImgUrl = event.target.dataset.source;
+    openModal();
+}
+
+console.log(bigImgUrl);
+const openModal = (event) => {
+    modalImg.setAttribute('src', `${bigImgUrl}`)
+    modal.classList.add('is-open');    
+}
+
+const gallaryClose = (event) => {
+    modalImg.removeAttribute('src');
+    modal.classList.remove('is-open');
+}
+
+const btn = document.querySelector('.lightbox__button');
+btn.addEventListener('click', gallaryClose)
+
+listRef.addEventListener('click', getBigUrl);
